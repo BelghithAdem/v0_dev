25528f78fe46541c2ee81bcfd7b77205d5224d29
Belghith Adem <123882675+BelghithAdem@users.noreply.github.com>
2025-01-13 23:21:07 +0100

    fix bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7359a6843ec17203"; }
+/******/ 		__webpack_require__.h = function() { return "c21cf55ac6d62436"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
